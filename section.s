5ec7fe0b45c1950d0d6a08db82bce5b6b88e8f03
tomocy <tomocy.dev@gmail.com>
2020-11-12 23:26:24 +0900

    Use sections

---
+++ section.s
@@ -0,0 +1,25 @@
+.text
+    .byte 0xaa
+.data
+    .byte 0xbb
+.bss
+.section .rodata
+    .byte 0xcc
+.section s0
+l0:
+    .byte 0x41
+    .byte 0x42
+    .byte 0x43
+    .byte 0x44
+.section s1
+l1:
+    .byte 0x45
+    .byte 0x46
+    .byte 0x47
+    .byte 0x48
+.section s0
+l2:
+    .byte 0x49
+    .byte 0x4a
+    .byte 0x4b
+    .byte 0x4c
